cd0126769807c8b28f6db7af9984b8bf2adce5a2
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-27 19:39:41 +0900

    匿名ログインをViewControllerからNextViewControllerへ値を渡しての画面遷移

--- FirebaseB/NextViewController.swift
+++ FirebaseB/NextViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 import Photos
+import Firebase
+
+/*
+
+課題
+
+画面を3つ準備し、(ABC)
+
+A.匿名ログイン後、Bへ画面遷移、DBへユーザー名とuserIDを入れ準備をする
+B.画像をアルバムからとって来れるようにし、自分のアイコンとして画像として設定。
+自分のDBへユーザー名、userID、画像URLを送信
+C.プロフィールページとして、DBから自分のユーザー名、userID、画像を取得し、表示
+
+*/
 
 class NextViewController: UIViewController,UIImagePickerControllerDelegate {
+    
+    
+    //匿名ログインIDをViewControllerからとってくる
+    var passedUserID = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +71,13 @@ class NextViewController: UIViewController,UIImagePickerControllerDelegate {
             print("エラー")
         }
     }
+    
+    
+    
+
+    
+    
+    
     
 
     /*
