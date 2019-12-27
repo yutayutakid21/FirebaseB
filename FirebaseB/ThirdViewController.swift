147888b03bd2a4e628c9bfebc737e3855fc1b6b3
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-27 19:07:44 +0900

    ViewControllerを３つ実装

---
+++ FirebaseB/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  FirebaseB
+//
+//  Created by 宏輝 on 27/12/2019.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
