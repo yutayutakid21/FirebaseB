810b5c6b70bbeb9c0e5e4387cc368ff97949c21d
Yuta Fujii <yutayutakid21@gmail.com>
2019-12-27 16:49:13 +0900

    Initial Commit

---
+++ FirebaseB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseB
+//
+//  Created by Fujii Yuta on 2019/12/27.
+//  Copyright © 2019 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
