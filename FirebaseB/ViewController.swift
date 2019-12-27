62c01e6dddb7f17060a27b0c9a02259dcb6941c3
Yuta Fujii <yutayutakid21@gmail.com>
2019-12-27 17:01:26 +0900

    OK

--- FirebaseB/ViewController.swift
+++ FirebaseB/ViewController.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+
+/*
+ 
+ 課題
+ 
+ 画面を3つ準備し、(ABC)
+ 
+ A.匿名ログイン後、Bへ画面遷移、DBへユーザー名とuserIDを入れ準備をする
+ B.画像をアルバムからとって来れるようにし、自分のアイコンとして画像として設定。
+ 自分のDBへユーザー名、userID、画像URLを送信
+ C.プロフィールページとして、DBから自分のユーザー名、userID、画像を取得し、表示
+ 
+ */
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
