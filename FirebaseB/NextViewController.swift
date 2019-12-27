b9e7318fc2483b622847fdf314a9d97a29abe9b7
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-27 19:30:18 +0900

    アルバム使用を実装

--- FirebaseB/NextViewController.swift
+++ FirebaseB/NextViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import Photos
 
-class NextViewController: UIViewController {
+class NextViewController: UIViewController,UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //appleは「カメラを使っても良いですか？」とユーザーに問いかけなければならないようにしている。
+        //ユーザーに画像ライブラリーの許可を促す。
+        PHPhotoLibrary.requestAuthorization { (status) in
+            
+            switch status{
+                
+            case .authorized:
+                print("許可されています。")
+            case .denied:
+                print("拒否された")
+            case .notDetermined:
+                print("notDetermined")
+            case .restricted:
+                print("restricted")
+                
+            }
+        }
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func openAlbum(_ sender: Any) {
+        
+        let sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            
+            let albumPicker = UIImagePickerController()
+            albumPicker.sourceType = sourceType
+            albumPicker.delegate = self
+            albumPicker.allowsEditing = true
+            present(albumPicker, animated: true, completion: nil)
+            
+        } else {
+            print("エラー")
+        }
     }
     
 
